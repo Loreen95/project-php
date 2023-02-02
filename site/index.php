49655da9874be076423d9b08333754025ef3c6c5
Lisa <lisahakhoff@ziggo.nl>
2023-02-02 09:56:08 +0100

    My second commit

--- site/index.php
+++ site/index.php
@@ -12,7 +12,16 @@
 <body>
     <?php
     echo "<h1>I am a BIG fan of the Simpsons</h1>";
+    echo "<h2>Doh!</h2>";
     ?>
+
+    <img src="images/simpson.jpg" ;>
+
+    <div class="footer">
+        <?php
+        echo "<h1>I am even named the same as my favorite character!</h1>";
+        ?>
+    </div>
 </body>
 
 </html>
